9effd4bd18f32a178dc5dcc215d1e5561d6f02a5
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2025-05-17 15:37:21 -0300

    Create exemplo.sql

---
+++ exemplo.sql
@@ -0,0 +1,8 @@
+SELECT
+  *
+
+FROM `projeto-spotify-457320.dadoshistoricos.spotify`
+
+WHERE
+  track_id NOT IN ('7173596', '5080031', '5675634', '3814670',
+'1119309', '4586215', '4967469', '8173823')
